194f66c149c813270ee9d1fec9831544bf53a69c
shimins <shimins@NTNUIT2179.win.ntnu.no>
2013-03-07 15:30:39 +0100

    1

---
+++ KalenderProsjekt/src/TestView.java
@@ -0,0 +1,94 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+
+public class TestView {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestView window = new TestView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TestView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 138);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblBrukernavn = new JLabel("Brukernavn:");
+		GridBagConstraints gbc_lblBrukernavn = new GridBagConstraints();
+		gbc_lblBrukernavn.anchor = GridBagConstraints.EAST;
+		gbc_lblBrukernavn.insets = new Insets(0, 0, 5, 5);
+		gbc_lblBrukernavn.gridx = 1;
+		gbc_lblBrukernavn.gridy = 1;
+		panel.add(lblBrukernavn, gbc_lblBrukernavn);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblPassord = new JLabel("Passord:");
+		GridBagConstraints gbc_lblPassord = new GridBagConstraints();
+		gbc_lblPassord.anchor = GridBagConstraints.EAST;
+		gbc_lblPassord.insets = new Insets(0, 0, 0, 5);
+		gbc_lblPassord.gridx = 1;
+		gbc_lblPassord.gridy = 2;
+		panel.add(lblPassord, gbc_lblPassord);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 0, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 2;
+		gbc_passwordField.gridy = 2;
+		panel.add(passwordField, gbc_passwordField);
+	}
+
+}
