06cad1e393ae2bb1bb140940df11ae2f9131d1e7
haakondi <hakon.dissen@gmail.com>
2013-03-11 17:17:25 +0100

    DBconnection nesten ferdig. Jobber med controller

--- KalenderProsjekt/src/data/Person.java
+++ KalenderProsjekt/src/data/Person.java
@@ -1,13 +1,26 @@
 package data;
 
 public class Person {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String email;
+	private int phone;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	
+	
+	public Person(String email, int phone, String firstName,
+			String lastName, String username, String password) {
+		super();
+		this.email = email;
+		this.phone = phone;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
 	}
+	
+	
+	
 
 }
--- KalenderProsjekt/src/server/DBController.java
+++ KalenderProsjekt/src/server/DBController.java
@@ -1,7 +1,29 @@
 package server;
 
+import java.sql.SQLException;
+
+import data.Person;
+
 public class DBController {
 
+	private DBConnection dBConn;
 	
-
+	//Needs error handling
+	public DBController(){
+		dBConn = new DBConnection();
+		try {
+			dBConn.initialize();
+		} catch (ClassNotFoundException e) {
+		} catch (SQLException e) {
+		}
+	}
+	
+	public boolean addPerson(Person person){
+		String sql = "INSERT INTO Person (username, passwd, firstname, lastname, email, telephone)";
+		sql += "VALUES";
+		
+		return true;
+	}
+	
+	
 }
