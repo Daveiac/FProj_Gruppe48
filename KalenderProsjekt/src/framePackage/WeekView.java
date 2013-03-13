d6b1cc5adc17aed1762bb91e288560191f4476a9
frank-albert <cspram90@gmail.com>
2013-03-13 14:16:06 +0100

    The whole shit!

--- KalenderProsjekt/src/framePackage/WeekView.java
+++ KalenderProsjekt/src/framePackage/WeekView.java
@@ -1,6 +1,7 @@
 package framePackage;
 
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -11,8 +12,13 @@ import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class WeekView {
+@SuppressWarnings("serial")
+public class WeekView extends JPanel {
 	
 	private JPanel weekView;
 	private JLabel lbltid;
@@ -24,89 +30,56 @@ public class WeekView {
 	private JLabel lbllørdag;
 	private JLabel lblsøndag;
 	private Date date;
-	private DefaultView df;
 	private JLabel lbltime;
-	private JLabel tittle;
+	private JLabel title;
 	private Dato dato;
+
+	public WeekView() {
+
+		// Default week view
+		String[] columnNames = {"Tid", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"};
+		DefaultTableModel tableModel = new DefaultTableModel(columnNames, 24);
+		for (int i = 0; i < 24; i++) {
+			String time = i + ":00";
+			tableModel.setValueAt(time, i, 0);
+		}
+		
+		JTable tblWeeks = new JTable(tableModel) {
+			public boolean isCellEditable(int rowIndex, int columnIndex) {
+				return false;
+			}
+		};
+		JScrollPane scrollPane = new JScrollPane(tblWeeks);
+		scrollPane.setPreferredSize(scrollPane.getPreferredSize());
+		
+		add(scrollPane);
+	}
+
+//	public WeekView(DefaultView defV) {
+//		date = new Date();
+//		initialize();
+//		dato = new Dato();
+//	}
 	
 	public static void main(String[] args) {
-		WeekView wv = new WeekView(new DefaultView());
-		JFrame frame = new JFrame("week view test");
+		JFrame frame = new JFrame("Week View");
+		
+		WeekView weekPanel = new WeekView();
+		
+		
+		frame.setContentPane(weekPanel);
+		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(wv.getWeekView());
-		frame.setSize(600, 500);
 		frame.setVisible(true);
 	}
 
-	private JPanel getWeekView() {
-		return weekView;
-	}
-
-	public WeekView(DefaultView defV) {
-		df = defV;
-		date = new Date();
-		initialize();
-		dato = new Dato();
-	}
-	
-	private void initialize(){
-		weekView = new JPanel(new GridBagLayout());
-		GridBagConstraints weekViewConstraints = new GridBagConstraints();
-		
-		for (int i=0;i<24;i++){
-			lbltime = new JLabel(i+ ":00");
-			weekViewConstraints.gridx = 0;
-			weekViewConstraints.gridy = i+1;
-			weekView.add(lbltime, weekViewConstraints);
-		}
-		weekViewConstraints.insets = new Insets(0, 10, 0, 15);
-		lbltid = new JLabel("Uke");
-		weekViewConstraints.gridx = 0;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lbltid, weekViewConstraints);
-		
-		weekViewConstraints.insets = new Insets(0, 0, 0, 35);
-		lblmandag = new JLabel("Mandag");
-		weekViewConstraints.gridx = 1;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lblmandag, weekViewConstraints);
-		
-		lbltirsdag = new JLabel("Tirsdag");
-		weekViewConstraints.gridx = 2;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lbltirsdag, weekViewConstraints);
-		
-		lblonsdag = new JLabel("Onsdag");
-		weekViewConstraints.gridx = 3;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lblonsdag, weekViewConstraints);
-		
-		lbltorsdag = new JLabel("Torsdag");
-		weekViewConstraints.gridx = 4;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lbltorsdag, weekViewConstraints);
-		
-		lblfredag = new JLabel("Fredag");
-		weekViewConstraints.gridx = 5;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lblfredag, weekViewConstraints);
-		
-		lbllørdag = new JLabel("Lørdag");
-		weekViewConstraints.gridx = 6;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lbllørdag, weekViewConstraints);
-		
-		lblsøndag = new JLabel("Søndag");
-		weekViewConstraints.gridx = 7;
-		weekViewConstraints.gridy = 0;
-		weekView.add(lblsøndag, weekViewConstraints);
-		
-		
-	}
-	
-	public JLabel getTittle(){
-		tittle = new JLabel(dato.getWeek() + "," + dato.getYear());
-		return tittle;
+	/**
+	 * 
+	 * @return title
+	 */
+	public JLabel getTitle(){
+		title = new JLabel(dato.getWeek() + "," + dato.getYear());
+		return title;
 	}
 	
 	public String getFirstDayOfWeek(){
