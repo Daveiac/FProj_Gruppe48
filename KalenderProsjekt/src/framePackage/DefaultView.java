3594835037ba7d62ae805dd9290481feb7f4d065
Shimin <Shimin@78.91.31.125>
2013-03-13 11:43:41 +0100

    MonthView skal jeg skrive på nytt

--- KalenderProsjekt/src/framePackage/DefaultView.java
+++ KalenderProsjekt/src/framePackage/DefaultView.java
@@ -30,8 +30,7 @@ import javax.swing.JToggleButton;
 
 public class DefaultView extends JPanel {
 
-	// her er main frame work, som kan brukes på year og month views også
-
+	// her er main frame work, som kan brukes pï¿½year og month views ogsï¿½
 	private JFrame frame;
 	private JLabel lblday;
 	private JLabel lblcalendar;
@@ -89,7 +88,7 @@ public class DefaultView extends JPanel {
 		
 		calendar = new JToggleButton("Kalender");
 		calendar.setSelected(true);
-		meeting = new JToggleButton("møte");
+		meeting = new JToggleButton("mé´—e");
 		calendarSelect = new ButtonGroup();
 		calendarSelect.add(calendar);
 		calendarSelect.add(meeting);
@@ -127,7 +126,7 @@ public class DefaultView extends JPanel {
 		calendarVisible.addItem("David Hovind");
 		calendarVisible.addItem("Aina Elisabeth Thunestveit");
 		calendarVisible.addItem("Christoffer Pram");
-		calendarVisible.addItem("Håkon Dissen");
+		calendarVisible.addItem("HéŒµon Dissen");
 		calendarVisible.addItem("Vegar Lerpoll");
 		calendarVisible.setSelectedItem(null);
 		sharedCalendarContraints.gridx = 0;
@@ -158,8 +157,8 @@ public class DefaultView extends JPanel {
 		warningPanel.add(createWarning("shit just got real"),
 				varselPanelContraints);
 
-		// her i fra kommer selve dayView delen, tar vekk lbltid så kan dette
-		// brukes på andre frames
+		// her i fra kommer selve dayView delen, tar vekk lbltid sï¿½kan dette
+		// brukes pï¿½andre frames
 
 		timePanel = new JPanel(new GridBagLayout());
 		
@@ -189,11 +188,11 @@ public class DefaultView extends JPanel {
 		dayWeekMonthSelect = new ButtonGroup();
 		day = new JToggleButton("Dag");
 		week = new JToggleButton("Uke");
-		month = new JToggleButton("Månede");
+		month = new JToggleButton("MéŒ¸ede");
 		dayWeekMonthSelect.add(day);
 		dayWeekMonthSelect.add(week);
 		dayWeekMonthSelect.add(month);
-		//setter inn if her for hva som skal være selected
+		//setter inn if her for hva som skal véŽŸe selected
 		day.setSelected(true);
 		timePanelContraints.weightx = 0;
 		timePanelContraints.gridx = 1;
@@ -205,14 +204,14 @@ public class DefaultView extends JPanel {
 		timePanelContraints.gridx = 3;
 		timePanelContraints.gridy = 0;
 		timePanel.add(month, timePanelContraints);
-		
-		timePanelContraints.gridwidth = 5;
-		timePanelContraints.fill = GridBagConstraints.HORIZONTAL;
-		timePanelContraints.weightx = 1;
-		timePanelContraints.gridx = 0;
-		timePanelContraints.gridy = 1;
-		timePanel.add(mainView, timePanelContraints);
-		
+//		
+//		timePanelContraints.gridwidth = 5;
+//		timePanelContraints.fill = GridBagConstraints.HORIZONTAL;
+//		timePanelContraints.weightx = 1;
+//		timePanelContraints.gridx = 0;
+//		timePanelContraints.gridy = 1;
+//		timePanel.add(mainView, timePanelContraints);
+//		
 	}
 
 	private JPanel getFocusPanel() {
