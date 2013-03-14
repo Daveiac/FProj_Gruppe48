0b4155bb8313fba931413098cd8546747c056c9c
Daveiac <daveiac@hotmail.com>
2013-03-14 17:33:44 +0100

    minor changes ro aview

--- KalenderProsjekt/src/framePackage/AppointmentView.java
+++ KalenderProsjekt/src/framePackage/AppointmentView.java
@@ -1,10 +1,13 @@
 package framePackage;
 
-import java.awt.List;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
-import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import data.Meeting;
 import data.MeetingRoom;
@@ -17,7 +20,26 @@ public class AppointmentView {
 	
 	public AppointmentView(ArrayList<Meeting> meetings) {
 		this.meetings = meetings;
+		JPanel mainPanel = new JPanel();
+		JPanel meetingPanel = new JPanel();
+		JPanel legend = new JPanel();
 		
+		meetingPanel.setLayout(new GridBagLayout());
+		refreshMeetings();
+		
+	}
+
+	private void refreshMeetings() {
+		GridBagConstraints mc = new GridBagConstraints();
+		for (int i = 0; i < meetings.size(); i++) {
+			JLabel icon = new JLabel();
+			JLabel date = new JLabel();
+			JLabel time = new JLabel();
+			JLabel title = new JLabel();
+			JLabel location = new JLabel();
+			JButton moreBtn = new JButton();
+			
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -25,7 +47,7 @@ public class AppointmentView {
 		ArrayList<Person> members = new ArrayList<Person>();
 		Team team = new Team(0, null, members);
 		MeetingRoom room = new MeetingRoom(0);
-		Person creator = new Person(null, 00000000, "Dave", "Hov", "dave", "1234");
+		Person creator = new Person(null, 00000000, "Dav", "Hov", "dave", "1234");
 		members.add(creator);
 		long startTime = new GregorianCalendar(2013, 2, 14, 16, 30).getTimeInMillis();
 		long endTime = new GregorianCalendar(2013, 2, 14, 17, 30).getTimeInMillis();
