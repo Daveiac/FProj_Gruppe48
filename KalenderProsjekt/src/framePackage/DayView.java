20399253e0b4ca9a9a068adef8885a194ecfd73b
frank-albert <cspram90@gmail.com>
2013-03-17 18:31:43 +0100

    DayView and WeekView is DONE! \o/   (need to implement the model though)

--- KalenderProsjekt/src/framePackage/DayView.java
+++ KalenderProsjekt/src/framePackage/DayView.java
@@ -2,16 +2,12 @@ package framePackage;
 
 import java.awt.Dimension;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
+import java.util.*;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
-import data.Meeting;
-import data.MeetingRoom;
-import data.Person;
-import data.Team;
+import data.*;
 
 /**
  * This is the DayView Panel that shows the day planner.
@@ -23,7 +19,7 @@ public class DayView extends JPanel implements CalendarView {
 	private String title;
 	private DefaultTableModel tableModel;
 	private JTable dayTable;
-	String[] columnHeaders;
+	private String[] columnHeaders;
 
 	// ----- test code -----
 	ArrayList<Meeting> meetings;
@@ -58,7 +54,10 @@ public class DayView extends JPanel implements CalendarView {
 		meetings.add(new Meeting(0, "suppemøte4", "fuInHell", startTime, endTime, "This is a desc", team, room, creator));
 		startTime = new GregorianCalendar(2013, 2, 16, 12, 00).getTimeInMillis();
 		endTime = new GregorianCalendar(2013, 2, 16, 15, 30).getTimeInMillis();
-//		meetings.add(new Meeting(0, "suppemøte5", "careInHell", startTime, endTime, "This is a desc", team, room, creator));
+		meetings.add(new Meeting(0, "suppemøte5", "careInHell", startTime, endTime, "This is a desc", team, room, creator));
+		startTime = new GregorianCalendar(2013, 2, 17, 03, 00).getTimeInMillis();
+		endTime = new GregorianCalendar(2013, 2, 17, 04, 30).getTimeInMillis();
+		meetings.add(new Meeting(0, "suppemøte6", "w00t?", startTime, endTime, "This is a desc", team, room, creator));
 		// ----- test code end -----
 
 
@@ -77,7 +76,7 @@ public class DayView extends JPanel implements CalendarView {
 		columnHeaders[0] = "Tid";
 		tableModel = new DefaultTableModel(columnHeaders, quarters);
 
-		// Sets the clock every 15 minutes
+		// Sets the clock every 15 minutes in the 1st column
 		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
 		// Saves this hour and minute
 		int thisHourOfDay = calendar.get(GregorianCalendar.HOUR_OF_DAY);
@@ -128,7 +127,7 @@ public class DayView extends JPanel implements CalendarView {
 		tableModel.setColumnIdentifiers(columnHeaders);
 
 		// Creates the day data
-		createDay(tableModel, numberOfDays);
+		createDay(calendar, tableModel, numberOfDays);
 
 		// Sets the new week into the table
 		dayTable.setModel(tableModel);
@@ -148,21 +147,21 @@ public class DayView extends JPanel implements CalendarView {
 	/**
 	 * Creates the new day's data.
 	 */
-	public void createDay(DefaultTableModel tableModel, int dayOfWeek) {
+	public void createDay(GregorianCalendar calendar, DefaultTableModel tableModel, int dayOfWeek) {
 
 		for (int i = 0; i < tableModel.getRowCount(); i++) {
 			tableModel.setValueAt(null, i, dayOfWeek);
 		}
 
 		// Sets today's meetings
-		setMeetings(dayOfWeek, meetings);
+		setMeetings(calendar, tableModel, dayOfWeek, meetings);
 	}
 
 	/**
 	 * Attaches the meetings to the table.
 	 * @param meetings	a list of meeting objects
 	 */
-	public void setMeetings(int dayOfWeek, ArrayList<Meeting> meetings) {
+	public void setMeetings(GregorianCalendar calendar, DefaultTableModel tableModel, int dayOfWeek, ArrayList<Meeting> meetings) {
 
 		for (Meeting meeting : meetings) {
 			GregorianCalendar meetingCalendar = new GregorianCalendar();
@@ -192,7 +191,7 @@ public class DayView extends JPanel implements CalendarView {
 			}
 		}
 	}
-	
+
 	/**
 	 * Gets the day view.
 	 * @return this	The day view
--- KalenderProsjekt/src/framePackage/WeekView.java
+++ KalenderProsjekt/src/framePackage/WeekView.java
@@ -2,13 +2,10 @@ package framePackage;
 
 import java.awt.Dimension;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
-import data.*;
 
 /**
  * This is the WeekView Panel that shows the week planner.
@@ -20,12 +17,12 @@ public class WeekView extends JPanel implements CalendarView {
 	private String title;
 	private DefaultTableModel tableModel;
 	private JTable weekTable;
+	private String[] columnHeaders;
 	private DayView dayView;
-	String[] columnHeaders;
 
-	// ----- test code -----
-	ArrayList<Meeting> meetings;
-	// ----- test code end -----
+	//	// ----- test code -----
+	//	ArrayList<Meeting> meetings;
+	//	// ----- test code end -----
 
 	/**
 	 * Constructs the WeekView Panel.
@@ -76,7 +73,7 @@ public class WeekView extends JPanel implements CalendarView {
 		columnHeaders[0] = "Tid";
 		tableModel = new DefaultTableModel(columnHeaders, quarters);
 
-		// Sets the clock every 15 minutes
+		// Sets the clock every 15 minutes in the 1st column
 		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
 		// Saves this hour and minute
 		int thisHourOfDay = calendar.get(GregorianCalendar.HOUR_OF_DAY);
@@ -125,67 +122,29 @@ public class WeekView extends JPanel implements CalendarView {
 		int thisDayOfWeek = calendar.get(GregorianCalendar.DAY_OF_WEEK);
 		// Sets the day to Monday
 		int firstDayOfWeek = -calendar.get(GregorianCalendar.DAY_OF_WEEK) + 2;
+		// If Sunday (1st day of the week in the Gregorian calendar), then go back to previous week
+		if (thisDayOfWeek == 1)
+			firstDayOfWeek -= 7;
 		calendar.add(GregorianCalendar.DAY_OF_WEEK, firstDayOfWeek);
 
-		// Sets table headers with corresponding days
+		// Sets table headers with corresponding days and creates the week data
 		SimpleDateFormat weekFormat = new SimpleDateFormat("EEEE dd. MMM.");
-		int daysInWeek = 8;
-		for (int dayOfWeek = 1; dayOfWeek < daysInWeek; dayOfWeek++) {
+		int daysInWeek = 7;
+		for (int dayOfWeek = 1; dayOfWeek <= daysInWeek; dayOfWeek++) {
 			columnHeaders[dayOfWeek] = weekFormat.format(calendar.getTime());
-			tableModel.setColumnIdentifiers(columnHeaders);
-			dayView.createDay(tableModel, dayOfWeek);
+			dayView.createDay(calendar, tableModel, dayOfWeek);
 			calendar.add(GregorianCalendar.DAY_OF_MONTH, 1);
 		}
+		tableModel.setColumnIdentifiers(columnHeaders);
 
 		// Sets the day today
-		thisDayOfWeek -= 9;
+		// If Sunday (1st day of the week in the Gregorian calendar), then go forth to this week
+		if (thisDayOfWeek == 1)
+			thisDayOfWeek -= 2;
+		else
+			thisDayOfWeek -= 9;
+
 		calendar.add(GregorianCalendar.DAY_OF_WEEK, thisDayOfWeek);
-
-
-		//		// Creates the week data
-		//		CreateWeek();
-
-		// Sets the new week into the table
-		weekTable.setModel(tableModel);
-		weekTable.getColumnModel().getColumn(0).setPreferredWidth(0);
-	}
-
-	/**
-	 * Sets this week's title.
-	 */
-	public void setWeekTitle() {
-		SimpleDateFormat titleFormat = new SimpleDateFormat("ww, MMMMM yyyy");
-		title = "Uke " + titleFormat.format(calendar.getTime());
-	}
-
-	public void CreateWeek() {
-
-
-		//		// Creates a table model
-		//		int quarters = 24 * 4;
-		//		tableModel = new DefaultTableModel(days, quarters);
-		//
-		//		// Sets the clock every 15 minutes
-		//		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
-		//		// Saves this hour and minute
-		//		int thisHourOfDay = calendar.get(GregorianCalendar.HOUR_OF_DAY);
-		//		calendar.add(GregorianCalendar.HOUR_OF_DAY, -thisHourOfDay);
-		//		int thisMinuteOfDay = calendar.get(GregorianCalendar.MINUTE);
-		//		calendar.add(GregorianCalendar.MINUTE, -thisMinuteOfDay);
-		//
-		//		for (int i = 0; i < quarters; i++) {
-		//			String time = timeFormat.format(calendar.getTime());
-		//			tableModel.setValueAt(time, i, 0);
-		//			calendar.add(GregorianCalendar.MINUTE, 15);
-		//		}
-		//
-		//		// Sets this hour and minute
-		//		int hoursOfDay = 23;
-		//		thisHourOfDay -= hoursOfDay;
-		//		calendar.add(GregorianCalendar.HOUR_OF_DAY, thisHourOfDay);
-		//		int minutesOfHour = 60;
-		//		thisMinuteOfDay -= minutesOfHour;
-		//		calendar.add(GregorianCalendar.MINUTE, thisMinuteOfDay);
 
 		// Sets the new week into the table
 		weekTable.setModel(tableModel);
@@ -198,6 +157,13 @@ public class WeekView extends JPanel implements CalendarView {
 		}
 	}
 
+	/**
+	 * Sets this week's title.
+	 */
+	public void setWeekTitle() {
+		SimpleDateFormat titleFormat = new SimpleDateFormat("ww, MMMMM yyyy");
+		title = "Uke " + titleFormat.format(calendar.getTime());
+	}
 
 	/**
 	 * Generates the title of week panel.
