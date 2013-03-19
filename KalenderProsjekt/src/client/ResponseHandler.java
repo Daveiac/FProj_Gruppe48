dd76af8ead3f2d3df9570d1ce5697ee7d50aa364
davidjh <davidjh@NTNUIT2178.win.ntnu.no>
2013-03-19 14:33:44 +0100

    added sysout in responsehandler

--- KalenderProsjekt/src/client/ResponseHandler.java
+++ KalenderProsjekt/src/client/ResponseHandler.java
@@ -1,13 +1,12 @@
 package client;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 
 import networking.packages.AuthenticationResponse;
 import networking.packages.AuthenticationResponse.AuthenticationResponseType;
-import networking.packages.DataResponse;
-import networking.packages.Response;
 import networking.packages.*;
 import data.*;
 
@@ -39,6 +38,7 @@ public class ResponseHandler implements Runnable{
 	
 	private void receivedMeeting(List<Meeting> data) {
 		List<Meeting> meetings = new ArrayList<Meeting>();
+		System.out.println("Meetings: "+Arrays.toString(meetings.toArray()));
 		for (Object object : data) {
 			meetings.add((Meeting) object);
 		}
