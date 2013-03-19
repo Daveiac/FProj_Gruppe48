637b0dd89a702b27078514a1682715bec1e1472e
davidjh <davidjh@NTNUIT2178.win.ntnu.no>
2013-03-19 13:39:10 +0100

    Fixed username bug in sql query

--- KalenderProsjekt/src/server/DBController.java
+++ KalenderProsjekt/src/server/DBController.java
@@ -154,7 +154,8 @@ public class DBController {
 		List<Meeting> meetings = new ArrayList<Meeting>();
 
 		String sql = "SELECT * FROM Meeting, Person "
-				+ "WHERE Meeting.username = '" + person.getUsername()+"';";
+				+ "WHERE Meeting.username = '" + person.getUsername()+"' "
+				+ "AND Person.username = '" + person.getUsername()+"';";
 		ResultSet rs = dBConn.makeQuery(sql);
 
 		while (rs.next()) {
