34d989657078c6ddcab82317e8bc658e7fc08944
frank-albert <cspram90@gmail.com>
2013-03-20 16:33:59 +0100

    mouselistener dayview

--- KalenderProsjekt/src/framePackage/DayView.java
+++ KalenderProsjekt/src/framePackage/DayView.java
@@ -143,16 +143,17 @@ public class DayView implements CalendarView, PropertyChangeListener {
 		dayTable.getColumnModel().getColumn(0).setPreferredWidth(0);
 		dayTable.getColumnModel().getColumn(1).setPreferredWidth(718);
 
-//		dayTable.addMouseListener(new java.awt.event.MouseAdapter() {
-//		    @Override
-//		    public void mouseClicked(java.awt.event.MouseEvent evt) {
-//		        int row = dayTable.rowAtPoint(evt.getPoint());
-//		        int col = dayTable.columnAtPoint(evt.getPoint());
-//		        if (row >= 0 && col >= 0) {
-//		        	Møtvisning møtevisning = new Møtevisning();
-//		        }
-//		    }
-//		});
+		dayTable.addMouseListener(new java.awt.event.MouseAdapter() {
+		    @Override
+		    public void mouseClicked(java.awt.event.MouseEvent evt) {
+		        int row = dayTable.rowAtPoint(evt.getPoint());
+		        int col = dayTable.columnAtPoint(evt.getPoint()) - 1;
+		        if (row >= 0 && col >= 0) {
+		        	Meeting meeting = (Meeting) DayView.this.tableModel.getValueAt(row, col);
+		        	NewAppointmentView nav = new NewAppointmentView(meeting);
+		        }
+		    }
+		});
 		
 		dayTable.getColumnModel().getColumn(1)
 				.setCellRenderer(new DayTableCellRenderer(this.calendarModel));
