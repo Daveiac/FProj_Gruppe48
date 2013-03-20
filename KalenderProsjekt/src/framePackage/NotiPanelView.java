36507d05a833171a78d7ec0898c71d5694e92983
shimins <shimins@NTNUIT2448.win.ntnu.no>
2013-03-20 15:56:52 +0100

    test

--- KalenderProsjekt/src/framePackage/NotiPanelView.java
+++ KalenderProsjekt/src/framePackage/NotiPanelView.java
@@ -7,6 +7,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -22,14 +24,15 @@ import javax.swing.JScrollPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import data.CalendarModel;
 import data.Meeting;
 import data.MeetingRoom;
 import data.Notification;
 import data.Person;
 import data.Team;
 
-public class NotiPanelView extends JPanel{
-	
+public class NotiPanelView extends JPanel implements PropertyChangeListener {
+
 	private JFrame frame;
 	private JPanel varselPanel;
 	private JLabel lblWarning;
@@ -37,49 +40,61 @@ public class NotiPanelView extends JPanel{
 	private DefaultListModel listModel;
 	private List<Notification> notifications;
 	private AppointmentOverView appointOverView;
-	
-	public NotiPanelView(){
-		notifications = new ArrayList<Notification>();
+	private CalendarModel calendarModel;
+
+	public NotiPanelView(CalendarModel calendarModel) {
+		this.calendarModel = calendarModel;
 		initialize();
 	}
-	
-	private void initialize(){
+
+	private void initialize() {
+
 		varselPanel = new JPanel(new GridBagLayout());
 		varselPanel.setPreferredSize(new Dimension(250, 300));
 		varselPanel.setVisible(true);
 		varselPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 		GridBagConstraints c = new GridBagConstraints();
-		
+
 		lblWarning = new JLabel("Varsel");
-		lblWarning.setFont(new Font(lblWarning.getFont().getName(),lblWarning.getFont().getStyle(), 20 ));
+		lblWarning.setFont(new Font(lblWarning.getFont().getName(), lblWarning
+				.getFont().getStyle(), 20));
 		c.gridx = 0;
 		c.gridy = 0;
 		varselPanel.add(lblWarning, c);
-		
-		listModel = new DefaultListModel();
+
+		listModel = new DefaultListModel<Notification>();
 		warningList = new JList<Notification>(listModel);
 		warningList.setFixedCellWidth(15);
 		warningList.setCellRenderer(new NotiViewRender());
 		JScrollPane scrollPane = new JScrollPane(warningList);
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane
+				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setPreferredSize(new Dimension(200, 250));
 		c.gridx = 0;
 		c.gridy = 1;
 		varselPanel.add(scrollPane, c);
 		warningList.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent arg0) {
-//				appointOverView = new AppointmentOverView(meeting, creator, notification);
-//				bruke calendarmodel her
+				Meeting meeting = ((Notification) warningList
+						.getSelectedValue()).getMeeting();
+				appointOverView = new AppointmentOverView(meeting);
 				appointOverView.getPanel();
 			}
-		}); 
-		
+		});
+
 	}
-	
-	
-	public JPanel getPanel(){
+
+	public JPanel getPanel() {
 		return varselPanel;
 	}
-	
+
+	public void propertyChange(PropertyChangeEvent evt) {
+		switch (evt.getPropertyName()) {
+		case CalendarModel.NOTIFICATIONS_CHANGED_Property:
+			notifications = calendarModel.getUnansweredNotificationsOfUser();
+			break;
+		}
+	}
 }
